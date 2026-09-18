1de63bda1bc57ed931035a9dd5a6ed2a8fb4d176
UditJain7125 <uditjain071@gmail.com>
2026-09-18 17:55:01 +0530

    Fix CORS for GitHub Pages

--- Backend/main.py
+++ Backend/main.py
@@ -85,7 +85,7 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173", "http://uditjain7125.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
